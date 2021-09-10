f32da0233c56c115c10d22012bea039abeae4afa
Rabin294 <bloomworld294@yahoo.com>
2021-09-10 14:56:57 +0800

    created project portfolio using create-react-app and built a about page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <div className="circle"></div>
+          <h1 className="title">Rabin Karki</h1>
+          <span className="subtitle">
+            Info: You can give any element “rounded corners” by applying a
+            border-radius through CSS. You’ll only notice if there is a color
+            change involved. For instance, if the element has a background-color
+            or border that is different than the element it’s over. Simple
+            examples: Info: You can give any element “rounded corners” by
+            applying a border-radius through CSS. You’ll only notice if there is
+            a color change involved. For instance, if the element has a
+            background-color or border that is different than the element it’s
+            over. Simple examples: Info: You can give any element “rounded
+            corners” by applying a border-radius through CSS. You’ll only notice
+            if there is a color change involved. For instance, if the element
+            has a background-color or border that is different than the element
+            it’s over. Simple examples: Info: You can give any element “rounded
+            corners” by applying a border-radius through CSS. You’ll only notice
+            if there is a color change involved. For instance, if the element
+            has a background-color or border that is different than the element
+            it’s over. Simple examples: Info: You can give any element “rounded
+            corners” by applying a border-radius through CSS. You’ll only notice
+            if there is a color change involved. For instance, if the element
+            has a background-color or border that is different than the element
+            it’s over. Simple examples:Info: You can give any element “rounded
+            corners” by applying a border-radius through CSS. You’ll only notice
+            if there is a color change involved. For instance, if the element
+            has a background-color or border that is different than the element
+            it’s over. Simple examples: Info: You can give any element “rounded
+            corners” by applying a border-radius through CSS. You’ll only notice
+            if there is a color change involved. For instance, if the element
+            has a background-color or border that is different than the element
+            it’s over. Simple examples:
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
