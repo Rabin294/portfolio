261e61d765acdcbe3ad27a6eaa23ceaabe8eba71
Rabin294 <bloomworld294@yahoo.com>
2021-09-12 10:57:18 +0800

    created blog page and decided to write down on the components for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./page/home-page/homepage.component";
 
 import Header from "./components/header/header.component";
+import HomePage from "./page/home-page/homepage.component";
 import Aboutpage from "./components/about-page/aboutpage.component";
-import Skillspage from "./components/skills-page/skills.component";
+import Skillspage from "./components/Skills/skills-page/skills.component";
+import Blogpage from "./components/blog-page/blog.component";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={Aboutpage} />
         <Route path="/skills" component={Skillspage} />
+        <Route path="/blog" component={Blogpage} />
         {/* <Route path="/projects" component={ProjectsPage} />
         <Route path="/skills" component={SkillsPage} />
         <Route path="/blog" component={BlogPage} />
--- src/components/about-page/aboutpage.component.jsx
+++ src/components/about-page/aboutpage.component.jsx
@@ -5,37 +5,39 @@ import aimage from "../../assets/bg-about.webp";
 import "./aboutpage.styles.scss";
 
 const Aboutpage = () => (
-  <div
-    style={{
-      backgroundImage: `url(${aimage})`,
-      backgroundSize: "cover",
-    }}
-    className="content"
-  >
+  <div className="about">
     <div
       style={{
-        backgroundImage: `url(${pimage})`,
+        backgroundImage: `url(${aimage})`,
         backgroundSize: "cover",
       }}
-      className="circle"
-    ></div>
-    <h1 className="title">Rabin Karki</h1>
-    <span className="subtitle">
-      My name is Rabin. I am a react developer.
-      <br /> I have been learning programming since 2019.
-      <br />
-      Let me know If you want to know more about me.
-      <br />
-      My name is Rabin. I am a react developer.
-      <br /> I have been learning programming since 2019.
-      <br />
-      Let me know If you want to know more about me.
-      <br />
-      My name is Rabin. I am a react developer.
-      <br /> I have been learning programming since 2019.
-      <br />
-      Let me know If you want to know more about me.
-    </span>
+      className="content"
+    >
+      <div
+        style={{
+          backgroundImage: `url(${pimage})`,
+          backgroundSize: "cover",
+        }}
+        className="circle"
+      ></div>
+      <h1 className="title">Rabin Karki</h1>
+      <span className="subtitle">
+        My name is Rabin. I am a react developer.
+        <br /> I have been learning programming since 2019.
+        <br />
+        Let me know If you want to know more about me.
+        <br />
+        My name is Rabin. I am a react developer.
+        <br /> I have been learning programming since 2019.
+        <br />
+        Let me know If you want to know more about me.
+        <br />
+        My name is Rabin. I am a react developer.
+        <br /> I have been learning programming since 2019.
+        <br />
+        Let me know If you want to know more about me.
+      </span>
+    </div>
   </div>
 );
 
