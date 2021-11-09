31e9054633468c9e37b763fd6b810391010e3ae2
Rabin294 <bloomworld294@yahoo.com>
2021-11-09 17:46:41 +0800

    added the link to card games project

--- src/components/projects-page/project.component.jsx
+++ src/components/projects-page/project.component.jsx
@@ -21,9 +21,9 @@ const Projectpage = () => (
           <a
             className="link"
             target="blank"
-            href="https://forkify-rabin.netlify.app"
+            href="https://rob-card-game.netlify.app/"
           >
-            <h3>Project forkify link</h3>
+            <h3>Card-game link</h3>
           </a>
 
           <span className="project-subtitle">
@@ -155,49 +155,6 @@ const Projectpage = () => (
             of blog traffic. The rise of Twitter and other "microblogging"
             systems helps integrate MABs and single-author blogs into the news
             media.
-          </span>
-        </div>
-      </div>
-      <div className="project-item">
-        <div className="project-content">
-          <h1 className="project-title">Project 4</h1>
-          <div
-            style={{
-              backgroundImage: `url(${cardgame})`,
-              backgroundSize: "cover",
-              height: "470px",
-            }}
-            className="image"
-          ></div>
-          <a
-            className="link"
-            target="blank"
-            href="https://forkify-rabin.netlify.app"
-          >
-            <h3>Project forkify link</h3>
-          </a>
-
-          <span className="project-subtitle">
-            A blog (a truncation of "weblog")[1] is a discussion or
-            informational website published on the World Wide Web consisting of
-            discrete, often informal diary-style text entries (posts). Posts are
-            typically displayed in reverse chronological order, so that the most
-            recent post appears first, at the top of the web page. Until 2009,
-            blogs were usually the work of a single individual,[citation needed]
-            occasionally of a small group, and often covered a single subject or
-            topic. In the 2010s, "multi-author blogs" (MABs) emerged, featuring
-            the writing of multiple authors and sometimes professionally edited.
-            MABs from newspapers, other media outlets, universities, think
-            tanks, advocacy groups, and similar institutions account for an
-            increasing quantity of blog traffic. The rise of Twitter and other
-            "microblogging" systems helps integrate MABs and single-author blogs
-            into the news media. Blog can also be used as a verb, meaning to
-            maintain or add content to a blog.A blog (a truncation of
-            "weblog")[1] is a discussion or informational website published on
-            the World Wide Web consisting of discrete, often informal
-            diary-style text entries (posts). Posts are typically displayed in
-            reverse chronological order, so that the most recent post appears
-            first, at the top of the web page.
           </span>
         </div>
       </div>
